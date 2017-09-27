6c4d191504c854c7c889ed84a7406bc57e37533b
Joseph990618 <30561976+Joseph990618@users.noreply.github.com>
2017-09-28 01:32:53 +0800

    last task

--- Joseph/task8.php
+++ Joseph/task8.php
@@ -1,10 +1,34 @@
 <?php
 
-$inputHours = (int)readline('How many hours do you work?' . PHP_EOL);
+$inputWorkAs = readline('Do you work as Part Time? True or False?' . PHP_EOL);
+$workingHours = (int)readline('In total, how many hours do you work?' . PHP_EOL);
 
-$fullTimeHour = 40;
-$fullTimeSalary = 2000;
+if ($inputWorkAs === True) {
+	
+	$partTimeSalary = 10;
 
-if ($inputHours > 40) {
-	$otHours = 40 % $inputHours;
+	$totalAmount = $workingHours * $partTimeSalary;
+
+	echo "Your salary is RM $totalAmount";
+
+} else {
+	
+	$fullTimeSalary = 2000;
+	$fullTimeHour = 160;
+	$otHourlyRate = 40;
+
+	if ($workingHours > $fullTimeHour) {
+		
+		$otHours = $workingHours % $fullTimeHour;
+
+		$otSalary = $otHours * $otHourlyRate;
+
+		$totalAmount = $fullTimeSalary + $otSalary;
+
+		echo "Your Salary is RM $totalAmount.";
+
+	} else {
+		echo "Your salary is RM $fullTimeSalary.";
+	}
+	
 }
