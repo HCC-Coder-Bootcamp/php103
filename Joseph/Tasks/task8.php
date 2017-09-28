d2f181fb8aa19560d5dbf12a2239b9c785b111db
Joseph990618 <30561976+Joseph990618@users.noreply.github.com>
2017-09-28 16:06:53 +0800

    hehehe

--- Joseph/Tasks/task8.php
+++ Joseph/Tasks/task8.php
@@ -10,62 +10,41 @@ $cutCostSalary  = 12.5;
 $partTimeRate   = 10;
 
 if ($inputWorkAs === 1) {
-	
 	if ($workingHours > 0 && $workingHours < $fullTimeHour) {
 		
 		$totalAmount = $workingHours * $partTimeRate;
-
 		echo "Your salary is RM $totalAmount.";
 
 	} else if ($workingHours > 0 && $workingHours >= $fullTimeHour) {
-
+		
 		$totalAmount = $workingHours * $partTimeRate;
-
 		echo "Your salary is RM $totalAmount. I would like you to join us as a full-time worker.";
 
 	} else if ($workingHours == 0) {
-		
 		echo "You are fired!!";
-
 	} else if ($workingHours < 0) {
-		
 		echo "Stop playing with me!";
-
 	}
-
 } else {
-	
 	if ($workingHours > $fullTimeHour) {
 		
 		$otHours = $workingHours % $fullTimeHour;
-
 		$otSalary = $otHours * $otHourlyRate;
-
 		$totalAmount = $fullTimeSalary + $otSalary;
-
 		echo "Your Salary is RM $totalAmount.";
 
 	} else if ($workingHours == $fullTimeHour) {
-		
 		echo "Your salary is RM $fullTimeSalary.";
-
 	} else if ($workingHours < $fullTimeHour && $workingHours > 0) {
 		
 		$lazyTime = $fullTimeHour - $workingHours;
-
 		$cutSalary = $lazyTime * $cutCostSalary;
-
 		$leftOverSalary = $fullTimeSalary - $cutSalary;
-
 		echo "Lazy People!! The boss cut your salary into RM $leftOverSalary.";
-
+		
 	} else if ($workingHours == 0) {
-		
 		echo "You are fired!!";
-
 	} else if ($workingHours < 0) {
-		
 		echo "Stop playing with me!";
-
 	}
 }
