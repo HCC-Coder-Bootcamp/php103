347a66e36b9abca2d66ba6d002338cf466e12209
Joseph990618 <30561976+Joseph990618@users.noreply.github.com>
2017-09-28 15:11:41 +0800

    left 3

--- Joseph/Homework/Homework1.php
+++ Joseph/Homework/Homework1.php
@@ -1,5 +1,37 @@
 <?php
 
 $userName     = readline('Username :' . PHP_EOL);
-$userPasswork = readline('Passwork :' . PHP_EOL);
+$userPassword = readline('Password :' . PHP_EOL);
 
+$userAlice = "alice";
+$passAlice = 12345;
+
+$userBob = "bob";
+$passBob = 23456;
+
+$userCindy = "cindy";
+$passCindy = "a2ncd";
+
+if ($userName == $userAlice) {
+	
+	if ($userPassword == $passAlice)
+		echo "Hello, Alice!";
+	else
+		echo "REJECT!";
+}
+
+if ($userName == $userBob) {
+	
+	if ($userPassword == $passBob)
+		echo "Hello, Bob!";
+	else
+		echo "REJECT!";
+}
+
+if ($userName == $userCindy) {
+	
+	if ($userPassword == $passCindy)
+		echo "Hello, Cindy!";
+	else
+		echo "REJECT!";
+}
