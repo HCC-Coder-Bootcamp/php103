e478cb565e806fe440dc5fe7ce1ebc1b8515cf61
davion111 <30561962+davion111@users.noreply.github.com>
2017-10-01 08:31:15 +0800

    backup

--- davion/task6.php
+++ davion/task6.php
@@ -1,5 +1,28 @@
 <?php
 
-$full_time = (echo "40hours/7 days")
+$hour = readline(' What is your working hour in per month?' . PHP_EOL);
+
+$working = readline (' What is your standard?' . PHP_EOL . '1 = OT pay' . PHP_EOL . '2 = part time pay' . PHP_EOL . '3 = full time pay plus OT pay' . PHP_EOL . '4 = full time pay only' . PHP_EOL . PHP_EOL);
+
+if ($working == 1){
+	$OT_hourly_rate = 40*$hour . PHP_EOL;
+	echo $OT_hourly_rate . PHP_EOL;
+} elseif ( $working == 2){
+	$part_time_hourly_rate = 10*$hour . PHP_EOL;
+	echo $part_time_hourly_rate . PHP_EOL;
+} elseif ( $working == 3){
+	$bonus_full_time_rate = (40*$hour)+2000 . PHP_EOL;
+	echo $bonus_full_time_rate . PHP_EOL;
+} elseif ( $working == 4){
+	echo 2000 . PHP_EOL;
+} elseif ( $working >=5 ){
+	echo error . PHP_EOL;
+}
+
+
+
+
+
+
 
 ?>
