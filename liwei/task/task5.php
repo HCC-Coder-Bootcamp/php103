aac156d6fd1294bb9b9253407702c45c779afe83
tlweiii6465 <tlweiii646564@gmail.com>
2017-10-03 08:58:59 +0800

    HA

--- liwei/task/task5.php
+++ liwei/task/task5.php
@@ -1,29 +1,25 @@
 <?php
 
-$Result = readline('Gimme your Result!!!' . PHP_EOL);
+$Grad = readline('Gimme a Grad!!!' . PHP_EOL);
 
-if ($Result >= 80 && $Result <= 100 ) {
+$Result = readline('And a Result!!!' . PHP_EOL);
 
-	echo "Passed! Gred A! Impressive!" . PHP_EOL;
+if ($Result > 100 OR $Result < 0 ) {
 
-} else if ($Result >= 70 && $Result <= 79 ) {
-
-	echo "Passed! Gred B! Good!" . PHP_EOL;
-
-} else if ($Result >= 60 && $Result <= 69 ) {
-
-	echo "Passes! Gred C! Nice!" . PHP_EOL;
-
-} else if ($Result >= 50 && $Result <= 59 ) {
-
-	echo "Passed! Gred C! Cool!" . PHP_EOL;
-
-} else if ($Result >= 39 && $Result >= 0 ) {
-
-	echo "Failed! Oh No! Keep Fighting!" . PHP_EOL;
+	echo "This is not a result" . PHP_EOL;
 			
 }else{
 
-	echo "Wrong result";
-	
+	if ($Grad == "A" AND $Result == "80" OR
+	    $Grad == "B" AND $Result == "70" OR
+	    $Grad == "C" AND $Result == "60" OR
+	    $Grad == "D" AND $Result == "50" ) {
+
+		echo "Passed";
+		
+	}else{
+
+		echo "Failed";
+
+	}
 }
