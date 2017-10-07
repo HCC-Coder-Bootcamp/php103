57840fab4e54748a1385afb8f4563f343452800e
Joseph990618 <30561976+Joseph990618@users.noreply.github.com>
2017-10-07 22:13:19 +0800

    done

--- Joseph/Homework/Homework4.php
+++ Joseph/Homework/Homework4.php
@@ -1,5 +1,20 @@
 <?php
 
-$inputYear = (int)readline('Please enter a year. I will let you know whether or not it is a leap year.' . PHP_EOL);
+echo "This is a Leap Year machine. I will let you know whether or not it is a Leap Year." . PHP_EOL;
 
-$leapYear = $inputYear / 4;
+$inputYear = (int)readline('Please enter a year.' . PHP_EOL);
+
+$leapYear  = $inputYear % 4;
+$leapYear1 = $inputYear % 100;
+$leapYear4 = $inputYear % 400;
+
+if ($leapYear == 0) {
+	if ($leapYear1 == 0 && $leapYear4 != 0) {
+		$messege = "This year is not a Leap Year.";	
+	} else {
+		$messege = "This year is a Leap Year.";
+	}
+} else {
+	$messege = "This Year is not a Leap Year.";
+}
+echo $messege;
