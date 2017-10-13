6f47514eec7ddad4e3ba11274dea93c674381f7a
tlweiii6465 <tlweiii646564@gmail.com>
2017-10-13 13:23:05 +0800

    ha

--- liwei/homework/homework2.php
+++ liwei/homework/homework2.php
@@ -12,46 +12,46 @@ $Number4 = readline('Number4 ' . PHP_EOL);
 
 $Number5 = readline('Number5 ' . PHP_EOL);
 
-if ($Number1 > $Number2 && $Number1 > $Number3) {
+if ($Number1 < $Number2 && $Number1 < $Number3) {
 
-	if ($Number1 > $Number4 && $Number1 > $Number5) {
+	if ($Number1 < $Number4 && $Number1 < $Number5) {
 		
-		echo "$Number1 is Largest";
+		echo "$Number1 is smallest";
 
-	}else if ($Number1 > $Number4 && $Number1 < $Number5) {
+	}else if ($Number1 < $Number4 && $Number1 > $Number5) {
 		
-		echo "$Number5 is Largest";
+		echo "$Number5 is smallest";
 
 	}else{
 
-		echo "$Number4 is Largest";
+		echo "$Number4 is smallest";
 
 	}
 
-}else if ($Number2 > $Number3 && $Number2 > $Number4) {
+}else if ($Number2 < $Number3 && $Number2 < $Number4) {
 
-	if ($Number2 > $Number5) {
+	if ($Number2 < $Number5) {
 		
-		echo "$Number5 is Largest";
+		echo "$Number5 is smallest";
 
 	}else{
 
-		echo "$Number2 is Largest";
+		echo "$Number2 is smallest";
 	}
 	
-}else if ($Number3 > $Number4 && $Number3 > $Number5) {
+}else if ($Number3 < $Number4 && $Number3 < $Number5) {
 
-	echo "$Number3 is Largest";
+	echo "$Number3 is smallest";
 
 }else{
 
-	 if ($Number4 > $Number5){
+	 if ($Number4 < $Number5){
 
-	 	echo "$Number4 is Largest"; 
+	 	echo "$Number4 is smallest"; 
 
 	 }else{
 
-	 	echo "$Number5 is Largest";
+	 	echo "$Number5 is smallest";
 
 	 }
 	
